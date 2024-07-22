8b4cec41eacbc27c94c918246ca558c083f76461
Shushant Kumar <74912161+Living-Hell@users.noreply.github.com>
2024-07-23 00:13:12 +0530

    Updating Resume

--- js/main.js
+++ js/main.js
@@ -1151,7 +1151,7 @@ function loadContent(projectData) {
 //Download Resume
 function downloadResume() {
   window.location.assign(
-    "https://drive.usercontent.google.com/u/0/uc?id=1JZJOosp1BZ_Lkkx4Hev1eIuJnly_0aNn&export=download"
+    "https://drive.usercontent.google.com/u/0/uc?id=1pLNl8BQPk-SuAxJzd6YGzSMWSsmHMM9P&export=download"
   );
 }
 
